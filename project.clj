fa8f9bf4ae44bc3beec5f685c93e40ec1e02af73
Simon Härer <simon.haerer@active-group.de>
2019-04-11 08:53:50 +0200

    Start new devel cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/active-viz "0.1.3"
+(defproject de.active-group/active-viz "0.2.1-SNAPSHOT"
   :description "Active Viz: ClojureScript SVG visualization tools"
   :url "https://github.com/active-group/active-viz"
   :license {:name "Eclipse Public License"
