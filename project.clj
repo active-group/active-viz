502f975f9bab0e0db1042a46aae835f4cbc48461
David Frese <david.frese@active-group.de>
2020-09-28 17:30:19 +0200

    Version 0.2.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/active-viz "0.2.8-SNAPSHOT"
+(defproject de.active-group/active-viz "0.2.8"
   :description "Active Viz: ClojureScript SVG visualization tools"
   :url "https://github.com/active-group/active-viz"
   :license {:name "Eclipse Public License"
